fd042e127420f8229352660b7b98924aecd57201
Edward Zhou <esz7923@nyu.edu>
2024-08-04 14:22:34 -0700

    add league spartan font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 import typography from "@tailwindcss/typography";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["League Spartan Variable", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [typography],
 };
