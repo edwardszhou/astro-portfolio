3c6b035952add852dc788e6a9a49509d3aa3a008
Edward Zhou <esz7923@nyu.edu>
2024-08-04 22:44:21 -0700

    add basic homepage

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -12,20 +12,20 @@ export default function Nav() {
       </div>
       {matches ? (
         <div className="flex h-full items-center gap-16 pr-16">
-          <a className="cursor-pointer rounded-full bg-red-300 px-6 py-3 font-medium text-red-900 transition-transform hover:scale-110">
+          <a className="cursor-pointer rounded-full bg-red-300 px-6 py-3 font-bold text-red-900 transition-transform hover:scale-110">
             Projects
           </a>
-          <a className="cursor-pointer rounded-full bg-yellow-200 px-6 py-3 font-medium text-yellow-900 transition-transform hover:scale-110">
+          <a className="cursor-pointer rounded-full bg-yellow-200 px-6 py-3 font-bold text-yellow-900 transition-transform hover:scale-110">
             About
           </a>
-          <a className="cursor-pointer rounded-full bg-green-200 px-6 py-3 font-medium text-green-900 transition-transform hover:scale-110">
+          <a className="cursor-pointer rounded-full bg-green-200 px-6 py-3 font-bold text-green-900 transition-transform hover:scale-110">
             Blog
           </a>
         </div>
       ) : (
         <div
           onClick={() => setNavOpen((isOpen) => !isOpen)}
-          className="group z-40 flex h-full w-8 cursor-pointer flex-col justify-center gap-1.5"
+          className="group z-50 flex h-full w-8 cursor-pointer flex-col justify-center gap-1.5"
         >
           <span className="h-0.5 w-full bg-black transition-all group-hover:bg-red-500" />
           <span className="h-0.5 w-3/4 bg-black transition-all group-hover:bg-yellow-500" />
@@ -33,7 +33,7 @@ export default function Nav() {
         </div>
       )}
       {navOpen && !matches && (
-        <div className="absolute inset-0 flex h-screen w-full flex-col items-center justify-center bg-white/80 backdrop-blur">
+        <div className="absolute inset-0 z-40 flex h-screen w-full flex-col items-center justify-center bg-white/80 backdrop-blur">
           <div className="flex flex-col items-center gap-16">
             <a className="cursor-pointer rounded-full bg-red-300 px-6 py-3 font-medium text-red-900 transition-transform hover:scale-110">
               Projects
